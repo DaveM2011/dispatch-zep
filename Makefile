c62cae9fb22b87095496c448e09c5145363a0016
Dave M <davem.2011@gmail.com>
2017-06-21 20:30:09 +0100

    Version 0.4

--- Makefile
+++ Makefile
@@ -1,12 +1,3 @@
 
 all:
 	zephir install
-
-key:
-	ssh-keygen -q -f app/priv.key -t rsa -b 1024 -N ""
-	rm -f app/priv.key.pub
-	openssl rsa -in app/priv.key -pubout > app/public.key
-
-run:
-	mkdir -p app/data
-	cd app && caddy
--- app/public/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$user = json_encode([3, "BigDave"]);
-$publickey_res = openssl_pkey_get_public("file://../public.key");
-if ($publickey_res === false) return null;
-openssl_public_encrypt($user, $encrypted, $publickey_res);
-$access_token = bin2hex($encrypted);
-header("Location: /?access_token=" . $access_token);
-?>
